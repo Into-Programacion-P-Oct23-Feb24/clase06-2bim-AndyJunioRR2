dd7c9668e5f69b19611fd33e8355adb3cbf2d074
AndyJunioRR2 <andyvein20@gmail.com>
2024-01-18 15:27:38 -0500

    Deber

--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -17,13 +17,11 @@ public class DatoTrabajo {
         String acumulador = "";
         System.out.println("Ingrese el nombre de la empresa");
         String empresa = entrada.nextLine();
-        System.out.println("Ingrese nombre de la empresa");
-        String nombreEmpresa = entrada.nextLine();
         System.out.println("Ingrese la dirección de la empresa");
         String direccionTrabajo = entrada.nextLine();
         acumulador = String.format("Nombre de la empresa es: %s\nDirección"
                 + " de la empresa es: %s",
-                nombreEmpresa,
+                empresa,
                 direccionTrabajo);
         return acumulador;
 
